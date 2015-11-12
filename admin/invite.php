6d003f09a63588250a7bbf6990d4c8bbd638d12b
28111600 <28111600@qq.com>
2015-11-12 11:14:57 +0800

    Update invite.php

--- admin/invite.php
+++ admin/invite.php
@@ -37,8 +37,8 @@ $code = $invite->CodeArray();
                                 </div>
 
                                 <div class="form-group">
-                                    <label for="cate_title">邀请码类别</label>
-                                    <input  class="form-control" name="code_type"  placeholder="0为公开，其他数字为对应用户的UID" >
+                                    <label for="cate_title">用户UID</label>
+                                    <input  class="form-control" name="code_type"  placeholder="用户的UID" >
                                 </div>
 
                                 <div class="form-group">
