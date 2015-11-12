19b482bb7e5a360badb172dfe7ce690b1533cf64
28111600 <28111600@qq.com>
2015-11-12 17:42:29 +0800

    Update header.php

--- header.php
+++ header.php
@@ -14,14 +14,15 @@
     <div class="nav-wrapper container"><a id="logo-container" href="/" class="brand-logo"><?php echo $site_name; ?></a>
         <ul class="right hide-on-med-and-down">
             <li><a href="index.php">首页</a></li>
-            <li><a href="client.php">客户端</a></li>
             <li><a href="user">用户中心</a></li>
+            <li><a href="client.php">客户端</a></li>
+
         </ul>
 
         <ul id="nav-mobile" class="side-nav">
             <li><a href="index.php">首页</a></li>
-            <li><a href="client.php">客户端</a></li>
             <li><a href="user">用户中心</a></li>
+            <li><a href="client.php">客户端</a></li>
         </ul>
         <a href="#" data-activates="nav-mobile" class="button-collapse"><i class="material-icons">menu</i></a>
     </div>
