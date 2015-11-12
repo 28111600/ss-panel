7512d010e3bc7d876c832b43da8abdd462840dcc
28111600 <28111600@qq.com>
2015-11-12 17:37:31 +0800

    Create client.php

---
+++ client.php
@@ -0,0 +1,92 @@
+<?php
+include_once 'lib/config.php';
+include_once 'header.php';
+?>
+
+
+<div class="section no-pad-bot" id="index-banner">
+<div class="container">
+            <br><br>
+            <h3 class="header">客户端</h1>
+            <div class="row">
+          <h4>OS X 客户端</h4>
+          <p>
+            </p><ul>
+              <li><a href="https://github.com/shadowsocks/shadowsocks-gui">shadowsocks-gui</a> - Cross-platform GUI powered by node and Webkit</li>
+              <li><a href="https://github.com/ohdarling/GoAgentX">GoAgentX</a> - OS X client, with GUI</li>
+		<li><a href="https://github.com/shadowsocks/shadowsocks-iOS/wiki/Shadowsocks-for-OSX-Help">Shadowsocks for Mac</a> - Shadowsocks GUI designed for OS X 10.7+</li>
+            </ul>
+          <p></p>
+
+          <h4>Windows 客户端</h4>
+          <p>
+            </p><ul>
+              <li><a href="https://goagentplus.com">GoAgent+</a> - Windows client</li>
+              <li><a href="https://github.com/shadowsocks/shadowsocks-gui">shadowsocks-gui</a> - Cross-platform GUI powered by node and Webkit</li>
+              <li><a href="https://github.com/clowwindy/shadowsocks-csharp">shadowsocks-csharp</a> - Windows version with GUI</li>
+              <li><a href="https://github.com/dallascao/yingwa">Yingwa</a> - Shadowsocks Windows client</li>
+            </ul>
+          <p></p>
+
+          <h4>Linux 客户端</h4>
+          <p>
+            </p><ul>
+              <li><a href="https://github.com/shadowsocks/shadowsocks-gui">shadowsocks-gui</a> - Cross-platform GUI powered node and by Webkit</li>
+              <li>Or use command line versions in <a href="/download.html">Server side</a></li>
+            </ul>
+          <p></p>
+
+          <h4>iOS 客户端</h4>
+          <p>
+            </p><ul>
+              <li><a href="https://github.com/shadowsocks/shadowsocks-iOS">shadowsocks-iOS</a> - All devices, web browser, global proxy with some restrictions</li>
+              <li><a href="https://github.com/linusyang/MobileShadowSocks">MobileShadowSocks</a> - Jailbroken devices only, global proxy with no restriction</li>
+            </ul>
+          <p></p>
+
+          <h4>Android 客户端</h4>
+          <p>
+            </p><ul>
+              <li><a href="https://github.com/shadowsocks/shadowsocks-android">shadowsocks-android</a></li>
+            </ul>
+          <p></p>
+
+          <h4>路由器 / Router 客户端</h4>
+          <p>
+            </p><ul>
+              <li><a href="https://github.com/haohaolee/shadowsocks-openwrt">shadowsocks-openwrt</a> - works on OpenWRT routers</li>
+              <li><a href="http://geewan.com?r=4012">极玩路由器【推荐】</a>
+              </li>
+            </ul>
+          <p></p>
+
+          <h4>实验环境</h4>
+          <p>
+            </p><ul>
+              <li><a href="https://github.com/clowwindy/shadowsocks-ruby">shadowsocks-ruby</a> Ruby version</li>
+              <li><a href="https://github.com/clowwindy/shadowsocks-chromeapp">shadowsocks-chromeapp</a> - Chrome App</li>
+            </ul>
+          <p></p>
+
+          <h4>其他支持 Shadowsocks 的上网方式</h4>
+          <p>
+            </p><ul>
+              <li><a href="https://github.com/cyfdecyf/cow">cow</a> - Powerful HTTP proxy, detecting blocked sites automatically</li>
+              <li><a href="https://github.com/jackyz/pobi">pobi</a></li>
+              <li><a href="https://github.com/clowwindy/lightsocks">lightsocks</a> - Transparent TCP proxy</li>
+              <li><a href="https://github.com/tuoxie007/Tweet4China2">Tweet4China2</a> - A Twitter client</li>
+            </ul>
+          <p></p>
+
+          <h4>其他版本（非官方）</h4>
+          <p>
+            </p><ul>
+              <li><a href="https://github.com/lerry/balan-proxy">balan-proxy</a></li>
+            </ul>
+          <p></p>
+        </div>
+            <br><br>
+        </div>
+</div>
+<?php  include_once 'ana.php';
+       include_once 'footer.php';?>
