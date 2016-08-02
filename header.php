84be5ac3ffae7c9aabe34eafd56ba842ea81dfce
28111600 <28111600@qq.com>
2016-08-02 16:57:41 +0800

    Update header.php

--- header.php
+++ header.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1.0"/>
     <title><?php echo $site_name; ?></title> 
     <!-- CSS fonts.googleapis.com -->
-    <link href="//fonts.lug.ustc.edu.cn/icon?family=Material+Icons" rel="stylesheet">
+    <link href="asset/css/MaterialIcons.css" rel="stylesheet">
     <link href="asset/materialize/css/materialize.min.css" type="text/css" rel="stylesheet" media="screen,projection"/>
     <link href="asset/materialize/css/style.css" type="text/css" rel="stylesheet" media="screen,projection"/>
 </head>
