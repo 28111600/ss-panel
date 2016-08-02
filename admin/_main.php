7d262db461032c4dacd7c7df3ead2a51b7d853aa
28111600 <28111600@qq.com>
2016-08-02 16:37:50 +0800

    Update _main.php

--- admin/_main.php
+++ admin/_main.php
@@ -118,12 +118,6 @@ require_once '_check.php';
                     </a>
                 </li>
                 
-                <li>
-                    <a href="sys.php">
-                        <i class="fa fa-align-left"></i> <span>系统信息</span>
-                    </a>
-                </li>
-
             </ul>
         </section>
         <!-- /.sidebar -->
